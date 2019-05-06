fe15d3a51bf9b54d5c6a3d2ebbcebe396e00a863
sanduhrs <sanduhrs@28074.no-reply.drupal.org>
2019-05-06 10:41:02 +0200

    Issue #3052790 by sanduhrs: Add default banner block

---
+++ modules/lcd_banner/src/Plugin/Block/DefaultBannerBlock.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\lcd_banner\Plugin\Block;
+
+/**
+ * Provides a 'Default Banner' block.
+ *
+ * @Block(
+ *   id = "lcd_banner_default",
+ *   admin_label = @Translation("Default Banner"),
+ *   category = @Translation("Banner")
+ * )
+ */
+class DefaultBannerBlock extends DrupalCommunityBlock {
+}
